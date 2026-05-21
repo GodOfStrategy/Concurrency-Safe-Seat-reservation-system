f4a997d69a1b5a02fc98c8a04b7fc42d8b8174b4
Shaurya Baitule <shaurya11.baitule@gmail.com>
2026-05-21 19:37:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
+const express = require('express');
+const reservationRoutes = require('./routes/reservationRoutes');
 
+require('./scheduler/cleanupScheduler');
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/api', reservationRoutes);
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
